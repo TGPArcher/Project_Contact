01ae4d00aaa7423b3634c52615a3bf6de50fcb6d
Petru Grozea <petrugrozea@gmail.com>
2018-01-11 09:09:38 +0200

    :construction: Canvas mouse events :sparkles: mouse functions

--- Console_Contact_List/c_events.c
+++ Console_Contact_List/c_events.c
@@ -2,6 +2,11 @@
 #include <stdio.h>
 #include <allegro5/allegro.h>
 
+void m_routine(ALLEGRO_EVENT_QUEUE*);
+void on_click(ALLEGRO_EVENT);
+void on_hover(ALLEGRO_EVENT);
+void on_scroll(ALLEGRO_EVENT);
+
 void m_events_init() {
 	ALLEGRO_EVENT_QUEUE *event_queue = NULL;
 
@@ -11,16 +16,37 @@ void m_events_init() {
 
 	al_register_event_source(event_queue, al_get_mouse_event_source());
 
+	m_routine(event_queue);
+}
+
+void m_routine(ALLEGRO_EVENT_QUEUE *event_queue) {
 	while (1) {
 		ALLEGRO_EVENT ev;
 		al_wait_for_event(event_queue, &ev);
 
-// 		if (ev.type == ALLEGRO_EVENT_MOUSE_BUTTON_UP)
-// 			if (ev.mouse.button == 1)
-// 				break;
+		if (ev.type == ALLEGRO_EVENT_MOUSE_BUTTON_UP) {
+			if (ev.mouse.button == 1)
+				on_click(ev);
+			else if (ev.mouse.button == 2)
+				break;
+		}
+		else if (ev.type == ALLEGRO_EVENT_MOUSE_AXES) {
+			if (ev.mouse.dx || ev.mouse.dy)
+				on_hover(ev);
+			else if (ev.mouse.dz)
+				on_scroll(ev);
+		}
 	}
 }
 
-// void on_click(ALLEGRO_EVENT ev) {
-// 	if(ev.type == ALLEGRO_EVENT_MOUSE_BUTTON_UP)
-// }
+void on_click(ALLEGRO_EVENT ev) {
+	printf("Click\n");
+}
+
+void on_hover(ALLEGRO_EVENT ev) {
+	printf("Hover\n");
+}
+
+void on_scroll(ALLEGRO_EVENT ev) {
+	printf("Scroll\n");
+}
